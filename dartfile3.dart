bd0d62ff96f734c1f1401dcbef2a47c97950a312
ShakirBaloch1 <shakirbaloch989@gmail.com>
2025-04-24 22:24:03 +0500

    Fixed bugs in the file

--- dartfile3.dart
+++ dartfile3.dart
@@ -1,21 +1,21 @@
 import 'dart:async';
 
 void main() {
-    String x = "10.5";
-    int y = double.parse(x).toInt();
-    print(y);
-    print(y.runtimeType);
-    String k = "10";
-    double m = int.parse(k).toDouble();
-    print(m);
-    print(m.runtimeType);
-    int b = 10;
-    String f = b.toString();
-    print(f);
-    print(f.runtimeType);
-    String h = "20.5";
-    int a = double.parse(h).toInt();
-    print(a);
+  String x = "10.5";
+  int y = double.parse(x).toInt();
+  print(y);
+  print(y.runtimeType);
+  String k = "10";
+  double m = int.parse(k).toDouble();
+  print(m);
+  print(m.runtimeType);
+  int b = 10;
+  String f = b.toString();
+  print(f);
+  print(f.runtimeType);
+  String h = "20.5";
+  int a = double.parse(h).toInt();
+  print(a);
   String name = "python programming";
   print(name.length);
   print(name);
@@ -23,7 +23,7 @@ void main() {
   print(x);
   String y = "python programming";
   print(y.substring(2, 10));
-   print(y.substring(7));
+  print(y.substring(7));
   print(y.substring(8));
   print(y.substring(1, 12));
   String z = ("substring  \'${y.substring(1, 10)}\'");
@@ -38,5 +38,5 @@ void main() {
   print(l.substring(0, 18));
   String string = '+5124343545';
   string = string.substring(0);
-   print(string);
-  
+  print(string);
+}
