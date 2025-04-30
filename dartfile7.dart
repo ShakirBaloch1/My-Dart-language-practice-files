0b4b61457561b1c60ac2aed76f0c6bea70125c7d
ShakirBaloch1 <shakirbaloch989@gmail.com>
2025-04-30 12:14:21 +0500

    dart operator

---
+++ dartfile7.dart
@@ -0,0 +1,40 @@
+void main() {
+  //int a = 20, a2 = 20;
+
+  /*print(a++);
+  print(a);
+  print(a2--);
+  print(a2);*/
+  /* print(++a);
+  print(a);
+  print(a++);
+  print(a);
+  String? name = "ali";
+  print(name);
+  int b = 22;
+  int z = 10;
+  print(b * z);
+  print(b / z);
+  print(b % z);
+print( b ~/z);*/
+  /*int b = 20;
+  print(b << 2);
+  print(b >> 2);
+  print(20 >= 10);
+  print(20 < 10);
+  print(10 == 10);
+  print(10 != 20);
+  print(10 != 10);*/
+  /* String name2 = "shakir";
+
+  print(name2 is String);
+  print("shakir" == name2);
+  print("shakir" != name2);
+  print("shakir" is! String);*/
+  int y = 20;
+  int z = 30;
+  print(y < z && y < z);
+  print(y < z && y > z);
+  print(y > z || y > z);
+  print(y < z || y > z);
+}
