066b386e4865c3d816a0798e721e8624ac5d3ca0
ShakirBaloch1 <shakirbaloch989@gmail.com>
2025-05-02 10:40:23 +0500

    if else topic

---
+++ dartfile9.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+void main() {
+  /*stdout.write("please enter a number");
+  int number = int.parse(stdin.readLineSync()!);
+  if (number > 0) {
+    print("positive number ${number}");
+  } else {
+    print("negative number ${number}");
+  }*/
+  /* stdout.write("please enter a number");
+  int number2 = int.parse(stdin.readLineSync()!);
+  if (number2 % 2 == 0) {
+    print("${number2} is even number");
+  } else {
+    print("${number2} is odd number");
+  }*/
+  /*stdout.write("please enter a marks");
+  int marks = int.parse(stdin.readLineSync()!);
+  if (marks > 90) {
+    print("${marks} marks are A grade");
+  } else if (marks > 80) {
+    print("${marks} marks are B grade");
+  } else if (marks > 60) {
+    print("${marks} marks are C grade");
+  } else {
+    print("you are fail");
+  }*/
+  /*stdout.write("please enter a number");
+  int marks = int.parse(stdin.readLineSync()!);
+  if (marks > 90) {
+    print("you are  A GRADE");
+    if (marks > 95) {
+      print("you win prize pool 10000");
+    }
+  }
+  else if ( marks <= 90) {
+    print("you are  b GRADE");
+    if (marks > 80) {
+      print("you win prize pool 1000");
+    }
+  } 
+  else {
+    print("you are fail");
+  }*/
+  /*String operators = stdin.readLineSync()!;
+  stdout.write("please enter a number");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("please enter a number");
+  int num2 = int.parse(stdin.readLineSync()!);
+  
+  if (operators == "+") {
+    print(num1 + num2);
+  } else if (operators == "-") {
+    print(num1 - num2);
+  } else if (operators == "%") {
+    print(num1 % num2);
+  }
+  else if (operators == "*") {
+    print(num1 * num2);
+  } 
+  else {
+    print("invalid operator");
+  }*/
+}
